a581a47d53eb84a20101bcc9b7fc9d5b83c107c6
Deepmj13 <128808826+Deepmj13@users.noreply.github.com>
2023-08-05 18:09:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,35 @@ const barEl = document.getElementById('bars');
 const barThree = document.getElementById('barthree');
 const barTwo = document.getElementById('bartwo');
 const barOne = document.getElementById('barone');
-
+const mobileBar = document.getElementById('mobile-nav1');
+const mobileBart = document.getElementById('mobile-nav2');
+const mobileBarth = document.getElementById('mobile-nav3');
+const mobileBarf = document.getElementById('mobile-nav4');
 	barEl.onclick = () =>{
 		navBar.classList.toggle('active');
 		barOne.classList.toggle('active');
 		barTwo.classList.toggle('active');
 		barThree.classList.toggle('active');
 	}
+	function close(){
+		navBar.classList.toggle('active');
+		barOne.classList.toggle('active');
+		barTwo.classList.toggle('active');
+		barThree.classList.toggle('active');
+	}
+	mobileBar.onclick = () => {
+		close()
+	}
+	mobileBart.onclick = () => {
+		close()
+	}
+	mobileBarth.onclick = () => {
+		close()
+	}
+	mobileBarf.onclick = () => {
+		close()
+	}
+
 
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
